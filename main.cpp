79d2d38ec55981e9fcf2c0dae748d369d9fa9253
mfurkaner <48824447+mfurkaner@users.noreply.github.com>
2021-10-20 17:15:38 +0300

    magnet1 icim optimizasyon denemesi

--- main.cpp
+++ main.cpp
@@ -5,49 +5,31 @@
 #include <math.h>
 #include <cstdlib>
 #include <string.h>
+#include <chrono>
+#include <vector>
 
 using namespace std;
+using namespace std::chrono;
 
+double Eradial(double r, double time, double phase);
 
-double Eradial(double r, double time, double phase){
+double bir_gecis(double r_pos, double vel, double Et, double t);
 
-//r is in mm
-//time is in ns, freq in MHz, so -9+6 = -3
-//phase in radians
-
- double Emax=0.960000;   // MVolts/m
- double freq=107.5; //MHz 
- double w=2*M_PI*freq/1000;
- //cout <<" r:"<<r<<" t:"<<time<<" ph:"<<phase<<"\n";
-
- double res=0;
-        res+=sin(w*time+phase)*((Emax/(1+(-200-r)/200))*(r < -188)*(r > -753)
-                               -(Emax/(1+(r-200)/200))*(r<753)*(r>188) );
-
-//      res+=cos(w*time+phase)*((Emax/(1+(740+r)/200))*(r < -188)*(r > -753)
-//                             +(Emax/(1+(740-r)/200))*(r<753)*(r>188) );
- return -res;
-} 
+double gecis(double r_pos, double vel, double Et, double &t);
 
 
 int main (int argc, char *argv[] ) {
 
-double Ein=40;   //keV
-double RFphase=15; // degrees
-double dT=0.1; //ns
-double R1=0.1882; //m
-double R2=0.753; //m
-double SimuTime=10; //ns
-double L1=0.4; // m, distane to first magnet
-double Rho1=0.23; //m, radius of the first magnet
-double L2=0.4; // m, distane to second magnet
-double Rho2=0.23; //m, radius of the second magnet
-double r_pos, vel, r_acc;
-double Relbeta, RelGamma;
+  auto start = high_resolution_clock::now();
 
-double dist=R2-R1;
-double Et=E0 + Ein/1000; // total energy in MeV
-r_pos=-R2;
+  double r_pos, vel;
+  double Relbeta;
+
+  double dist=R2-R1;
+  double Et=E0 + Ein/1000; // total energy in MeV
+  r_pos=-R2;
+
+  double t = 0;
 
 // find E field, calculate force,
 // calculate acceleration, accelerate particle
@@ -55,23 +37,92 @@ r_pos=-R2;
 // print some quantities
 // find new position: are we in E field, drift space of magnet?
 // did we exit?
+  
 
-for (double t=0; t<SimuTime; t+=dT){
-  vel = c*sqrt(Et*Et-E0*E0)/Et;
-  double RelBeta  = vel/c;
-  double RelGamma = 1.0 / sqrt(1.0-RelBeta*RelBeta);
-
-  double ef=Eradial(r_pos*1000,t,0); // convert position to mm
+  Et = gecis(r_pos, vel, Et, t);
   cout<<std::setprecision(3);
-  cout << "Beta i:"<<RelBeta<<" \tPos:"<<r_pos<< "(m) \tE:"<< ef<<"(MV/m) \tTime:"<< t<< "ns\t";
-  double acc=ef*1E6*eQMratio/(RelGamma*RelGamma*RelGamma); 
+  cout << "İlk gecis sonrasi enerji : " << Et-E0 << " MeV.  Cikis zamani : " << t << " ns" << endl;
+  double t_dum = t;
+  double t_opt;
+  double maxE = 0;
+  for(double i = 2; i < 9; i += dT_out){
+    t_dum += i;
+    double enow = gecis(r_pos, vel, Et, t_dum);
+    if( enow > maxE ){
+      maxE = enow;
+      t_opt = i;
+    }
+    t_dum = t;
+  }
+  cout<<std::setprecision(4);
+  cout << "İkinci geçiş sonrası maksimum enerji E = " << maxE - E0 << " MeV.  Dışarıda geçirdiği zaman : " << t_opt << " ns" << endl;
 
-  r_pos = r_pos + vel * dT*ns + 1/2*acc*(dT*ns)*(dT*ns);
-  vel=vel+acc*dT*ns;
-  RelBeta  = vel/c;
-  RelGamma = 1.0 / sqrt(1.0-RelBeta*RelBeta);
-  Et=RelGamma*E0; 
-  cout << "Beta f:"<<RelBeta<<" \tPos:"<<r_pos<< "(m) \tT:"<< Et-E0<<"(MeV) \n";
+  auto stop = high_resolution_clock::now();
+  auto duration = duration_cast<microseconds>(stop - start);
+  cout << "Simulation finished in : " << duration.count() << " mus" << endl;
+
+  return 0;
 }
 
+
+double Eradial(double r, double time, double phase){
+//r is in mm
+//time is in ns, freq in MHz, so -9+6 = -3
+//phase in radians
+
+  double w=2*M_PI*freq/1000;
+
+  double E_zero = sin(w*time+phase)*Emax*Emax_pos/r;
+
+  double res=( r < -R1*1000 && r > -R2*1000 ) ? E_zero : 0;
+
+  if( !res ){
+    res += ( r < R2*1000 && r > R1*1000 ) ? E_zero : 0;
+  }
+
+  return res;
+}
+
+double bir_gecis(double r_pos, double vel, double Et, double t){
+  for(; t<SimuTime; t+=dT){
+    vel = c*sqrt(Et*Et-E0*E0)/Et;
+    double RelBeta  = vel/c;
+    double RelGamma = 1.0 / sqrt(1.0-RelBeta*RelBeta);
+
+    double ef=Eradial(r_pos*1000,t,RFphase*deg_to_rad); // convert position to mm
+    //cout<<std::setprecision(3);
+    //cout << "Beta i:"<<RelBeta<<" \tPos:"<<r_pos<< "(m) \tE:"<< ef<<"(MV/m) \tTime:"<< t<< "ns\t";
+    double acc=ef*1E6*eQMratio/(RelGamma*RelGamma*RelGamma); 
+
+    r_pos = r_pos + vel * dT*ns + 1/2*acc*(dT*ns)*(dT*ns);
+    vel=vel+acc*dT*ns;
+    RelBeta  = vel/c;
+    RelGamma = 1.0 / sqrt(1.0-RelBeta*RelBeta);
+    Et=RelGamma*E0; 
+    //cout << "Beta f:"<<RelBeta<<" \tPos:"<<r_pos<< "(m) \tT:"<< Et-E0<<"(MeV) \n";
+  }
+  return Et;
+}
+
+double gecis(double r_pos, double vel, double Et, double &t){
+
+  for(; r_pos >= -R2 && r_pos <= R2 ; t+=dT){
+    vel = c*sqrt(Et*Et-E0*E0)/Et;
+    double RelBeta  = vel/c;
+    double RelGamma = 1.0 / sqrt(1.0-RelBeta*RelBeta);
+
+    double ef=Eradial(r_pos*1000,t,RFphase*deg_to_rad); // convert position to mm
+
+    //cout<<std::setprecision(3);
+    //cout << "Beta i:"<<RelBeta<<" \tPos:"<<r_pos<< "(m) \tE:"<< ef<<"(MV/m) \tTime:"<< t<< "ns\t";
+    double acc=ef*1E6*eQMratio/(RelGamma*RelGamma*RelGamma); 
+
+    r_pos = r_pos + vel * dT*ns + 1/2*acc*(dT*ns)*(dT*ns);
+    vel=vel+acc*dT*ns;
+    RelBeta  = vel/c;
+    RelGamma = 1.0 / sqrt(1.0-RelBeta*RelBeta);
+    Et=RelGamma*E0; 
+    //cout << "Beta f:"<<RelBeta<<" \tPos:"<<r_pos<< "(m) \tT:"<< Et-E0<<"(MeV) \n";
+  }
+  return Et;
 }
---
+++ rhodo.h
@@ -0,0 +1,29 @@
+
+const double c = 2.99792458e8; //m/s
+const double E0=0.511;  // MeV
+const double ns=1E-9; //ns conversion
+
+const double deg_to_rad = 0.01745;
+
+const double emass = 9.10938356e-31;  // kg
+const double echarge = 1.60217662e-19; //coulomb
+const double eQMratio = -1.75882e11; // C/kg
+
+
+#define Emax 0.960000 // MVolts/m
+#define freq 107.5    // MHz
+#define Ein 40.0      // KeV
+#define RFphase 15    // degree
+#define dT 0.001        // ns
+#define R1 0.1882     // m
+#define R2 0.753      // m
+#define SimuTime 10   // ns
+#define Emax_pos 200 // mm (Emax radial position)
+
+#define L1 0.4        // m (distance to first magnet)
+#define Rho1 0.23     // m (radius of the first magnet)
+
+#define L2 0.4        // m (distance to second magnet)
+#define Rho2 0.23     // m (radius of the second magnet)
+
+#define dT_out 0.01    // ns

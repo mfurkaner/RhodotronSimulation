f8d1f88087d9636e6f89c0fe0a339015782d89f9
Furkan <mfurkaner.mfe@gmail.com>
2021-10-21 15:33:51 +0300

    .

--- inc/rhodo.h
+++ inc/rhodo.h
@@ -19,7 +19,6 @@ const double eQMratio = -1.75882e11; // C/kg
 #define Emax 0.960000 // MVolts/m
 #define freq 107.5    // MHz
 #define Ein 40.0      // KeV
-#define RFphase 0    // degree
 #define dT 0.001        // ns
 #define R1 0.1882     // m
 #define R2 0.753      // m
@@ -33,7 +32,6 @@ const double eQMratio = -1.75882e11; // C/kg
 #define Rho2 0.23     // m (radius of the second magnet)
 
 #define dT_out 0.01    // ns
-#define dD_out 
 
 #define NUM_OF_ELECTRONS    50
 #define GUN_ACTIVE_TIME     1.0 // ns
@@ -42,7 +40,7 @@ const double eQMratio = -1.75882e11; // C/kg
 
 double Eradial(double r, double time, double phase);
 
-double vel_to_dist(double vel, double t);   // returns nm
+double vel_to_dist(double vel, double t);
 
 double dist_to_time(double dist, double vel);
 
@@ -50,6 +48,7 @@ double bir_gecis(double r_pos, double Et, double t);
 
 double gecis(double r_pos, double Et, double &t);
 
+void displayHelp();
 
 
 typedef pair<double, double> giris_cikis_tpair;
@@ -58,12 +57,16 @@ class Electron{
 public:
     double Et = E0 + Ein/1000;
     double r_pos = -R2;
+    double RFphase=0;
     vector< double > enerjiler;
     vector< giris_cikis_tpair > t_giris_cikis;
 
     double get_vel();
+
     double get_travel_time(double dist);
+
     void print_electron_info();
+
     void e_gecis(double &t);
 };
 
@@ -71,6 +74,8 @@ typedef pair<double, double> max_energy_rms_pair;
 
 class Bunch{
 public:
+    Bunch( ){RFphase_bunch=0;}
+    Bunch( double d ){RFphase_bunch=d;} 
     Electron e[NUM_OF_ELECTRONS];
     int e_count = NUM_OF_ELECTRONS;
     double initial_length_ns = GUN_ACTIVE_TIME;
@@ -87,6 +92,7 @@ public:
     void bunch_gecis_t(double &t_delay_of_max);
     void bunch_gecis_d(double dist_out);
 private:
+    double RFphase_bunch;
     void bunch_ilk_gecis(double &t);
     void bunch_nth_gecis_t(double t_delay_of_max);
     void bunch_nth_gecis_d(double dist_out);
